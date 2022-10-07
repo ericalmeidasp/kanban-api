97f2c3458daec68f6b445b39e59735b02f8f083a
ericalmeidasp <ericalmeida.sp@gmail.com>
2022-10-07 12:45:43 -0300

    fix: ajustes em bugs

--- src/main/java/com/ada/dynamo/dto/request/QuadroRequest.java
+++ src/main/java/com/ada/dynamo/dto/request/QuadroRequest.java
@@ -2,7 +2,10 @@ package com.ada.dynamo.dto.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class QuadroRequest {
+    @NotBlank
     private String name;
 }
